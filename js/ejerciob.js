c2ad0e31c6bab77276e5044f32c7ff56f261d188
GuidoAquino <aquino.guido@hotmail.com>
2023-07-29 22:44:29 -0300

    agregando ejercicio b

---
+++ js/ejerciob.js
@@ -0,0 +1,22 @@
+console.log(
+" -------------------------------------------------------------------\n",
+"-------------------------- Ejercicio B ----------------------------\n",
+"-------------------------------------------------------------------\n"
+);
+
+var cantidadDeGatos = 5
+var cantidadDePasos = 11
+
+
+
+for(var gato = 1; gato <= cantidadDeGatos; gato ++){
+var mensaje = "Gato" + gato + ": 🐈"
+for (var pasos = 1; pasos <= cantidadDePasos; pasos++ ){
+    mensaje += " 🐾";
+
+
+}
+
+console.log(mensaje)
+
+}
